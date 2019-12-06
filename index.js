410154ce7c46fa71ff87b57658eb2c0a7522622a
Albert Smit <albertsmit93@gmail.com>
2019-12-06 16:41:43 +0100

    converted gzip to stream

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const config = require('config');
 const WebSocket = require('ws');
 const zlib = require('zlib');
+const stream = require('stream');
+const miss = require('mississippi');
+const meter = require("stream-meter");
 
 const ip = config.get("ip");
 const port = config.get("port");
@@ -16,6 +19,10 @@ const topic = config.get("mqtttopic");
 const ws = new WebSocket('ws://' + ip + ':' + port + '/signalk/v1/stream?subscribe=none');
 var buffer = "[";
 var count = 0;
+var umcompressed;
+var compressed;
+var m;
+var gzip;
 ws.on('open', function open() {
     // subscribe to all signalk paths from configuration
     var subscriptionPaths = [];
@@ -39,9 +46,13 @@ ws.on('message', function incoming(data) {
     if (!message.roles) {
         if (count > 0) {
             buffer += ",";
+            uncompressed.push(',');
         }
         buffer += data;
+        uncompressed.push(data);
         count++;
+        console.log("length: ", m.bytes);
+        // gzip.flush();
         if (buffer.length > buffersize) {
             clearTimeout(timer);
             push();
@@ -59,15 +70,36 @@ function callback(err) {
     }
 }
 
+function mqttpublish(data, temp) {
+    console.log("payload: ", data.length);
+    console.log("length2: ", temp.bytes);
+    client.publish(topic, data, {
+        "qos": 2,
+    }, callback);
+}
+
+function createPipeline(payload) {
+    gzip = zlib.createGzip();
+    uncompressed = new stream.Readable();
+    uncompressed._read = () => {}; // see https://stackoverflow.com/a/22085851
+    compressed = miss.concat(res => mqttpublish(res, m));
+    m = meter();
+    uncompressed.pipe(gzip).pipe(m).pipe(compressed);
+    uncompressed.push("[");
+
+}
+
 function push() {
     // process the buffer and reset buffer and timer afterwards
     console.log("buffer", buffer.length);
-    var payload = zlib.gzipSync(buffer + "]");
-    console.log("payload: ", payload.length);
+    uncompressed.push("]");
+    uncompressed.push(null);
+    // var payload = zlib.gzipSync(buffer + "]");
+    // console.log("payload: ", payload.length);
     console.log("count: ", count);
-    client.publish(topic, payload, {
-        "qos": 2,
-    }, callback);
+    createPipeline();
+
     timer = setTimeout(push, period * 1000);
 }
+createPipeline();
 var timer = setTimeout(push, period * 1000);
